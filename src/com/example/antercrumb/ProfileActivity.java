c548c404fb7081130d7233106ac0282e1cac6b2b
Chai <menin88@gmail.com>
2013-10-31 16:10:08 +0100

    profileActivity added

---
+++ src/com/example/antercrumb/ProfileActivity.java
@@ -0,0 +1,32 @@
+package com.example.antercrumb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile);
+		
+	}
+	
+	public void onStart(){
+		super.onStart();
+		
+	}
+	public void onPause(){
+		super.onPause();
+		
+	}
+	public void onResume(){
+		super.onResume();
+		
+	}
+	public void onDestroy(){
+		super.onDestroy();
+		
+	}
+
+}
