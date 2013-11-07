1875af7c7bcf54cfa8d2bb29d0c6157890ad9fbb
stefno <shark90@fastwebnet.it>
2013-11-07 18:01:11 +0100

    playerEntity 2

---
+++ src/com/example/antercrumb/utils/PlayerEntity.java
@@ -0,0 +1,24 @@
+package com.example.antercrumb.utils;
+
+import com.google.api.client.json.GenericJson;
+import com.google.api.client.util.Key;
+
+public class PlayerEntity extends GenericJson{
+	
+	@Key
+	private String username;
+	@Key
+	private int imageID;
+	@Key
+	private String email;
+	public PlayerEntity (){}
+	
+	
+	//serve per ogni richiesta al database, serve per collegarti
+	/*scoreAppData = mKinveyClient.appData("Player", PlayerEntity.class);
+
+	dove scoreAppData è: private AsyncAppData<PlayerEntity> scoreAppData;
+    */
+	
+	
+}
